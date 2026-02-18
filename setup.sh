478eaa8c01a977a3f058a61e8d9c363aa00f5fc3
Patrick Hayes <pfhayes@gmail.com>
2026-02-17 17:21:06 -0800

    Isolate .osx setup

--- setup.sh
+++ setup.sh
@@ -9,18 +9,11 @@ fi
 
 OS="$(uname -s)"
 
+# OS-specific setup first
 if [ "$OS" = "Darwin" ]; then
-  # Xcode tools
-  if ! xcode-select -p; then
-    xcode-select --install
-  fi
-
-  # Homebrew
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" </dev/null
-  brew update
-  brew upgrade
-  xargs brew install <homebrew.packages
-  brew install --cask font-source-code-pro
+  "$BASE_PATH/.osx"
+elif [ "$OS" = "Linux" ]; then
+  "$BASE_PATH/.linux"
 fi
 
 # Submodules
@@ -65,18 +58,4 @@ fi
 # Vim plugins
 if command -v nvim &>/dev/null; then
   nvim --headless +PlugUpdate +qall
-fi
-
-# OS-specific preferences
-if [ "$OS" = "Darwin" ]; then
-  "$BASE_PATH/.osx"
-  cp "$BASE_PATH/com.apple.Terminal.plist" ~/Library/Preferences/com.apple.Terminal.plist
-  chsh -s /bin/zsh
-
-  echo 'TODO(patrick): Set mouse tap to click'
-  echo 'TODO(patrick): Update tracking speed'
-  echo 'TODO(patrick): Configure displays to optimize for retina display'
-  echo 'TODO(patrick): Set up SSH keys'
-elif [ "$OS" = "Linux" ]; then
-  "$BASE_PATH/.linux"
 fi
